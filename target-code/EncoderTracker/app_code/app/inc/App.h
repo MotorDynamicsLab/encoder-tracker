740d66b2d1108a375846fd5775fe294f4888a5dd
David Sun <david@ldomotors.com>
2017-12-12 20:54:38 +0800

    add code to track encoder counts internally

--- target-code/EncoderTracker/app_code/app/inc/App.h
+++ target-code/EncoderTracker/app_code/app/inc/App.h
@@ -18,12 +18,25 @@
 class App
 {
 private:
+	static const uint16_t num_encoders = 4;
+
+	enum EncoderIndex
+	{
+		_X1 = 0,
+		_X2 = 1,
+		_Y = 2,
+		_Z = 3,
+	};
+
+	Encoder encoderList[num_encoders];
+	int32_t encoderVals[num_encoders];
+	uint16_t oldEncoderCounts[num_encoders];
+
 	void ConfigEncoder();
 	void ConfigSpi();
 	void ConfigExtInt();
 public:
 	App();
-
 	void Initialize();
 	void Execute();
 	void ServeSpi();
--- target-code/EncoderTracker/app_code/app/src/App.cpp
+++ target-code/EncoderTracker/app_code/app/src/App.cpp
@@ -23,6 +23,52 @@ void App::Initialize()
 ///Configures the Timer modules to be used as encoders
 void App::ConfigEncoder()
 {
+	const uint16_t encoder_resolution = 0xFFFF;
+	Encoder::EncoderPinConfig pinConfig;
+
+	encoderList[_X1].Initialize(Encoder::_Tim4);
+	encoderList[_X2].Initialize(Encoder::_Tim1);
+	encoderList[_Y].Initialize(Encoder::_Tim3);
+	encoderList[_Z].Initialize(Encoder::_Tim2);
+
+	pinConfig.chAChannel = Gpio::_ChA;
+	pinConfig.chBChannel = Gpio::_ChA;
+	pinConfig.chAPin = 11;
+	pinConfig.chBPin = 12;
+	pinConfig.chAAltNum = 10;
+	pinConfig.chBAltNum = 10;
+	encoderList[_X1].EncoderPinInit(pinConfig);
+
+	pinConfig.chAChannel = Gpio::_ChA;
+	pinConfig.chBChannel = Gpio::_ChA;
+	pinConfig.chAPin = 8;
+	pinConfig.chBPin = 9;
+	pinConfig.chAAltNum = 6;
+	pinConfig.chBAltNum = 6;
+	encoderList[_X2].EncoderPinInit(pinConfig);
+
+	pinConfig.chAChannel = Gpio::_ChB;
+	pinConfig.chBChannel = Gpio::_ChB;
+	pinConfig.chAPin = 4;
+	pinConfig.chBPin = 5;
+	pinConfig.chAAltNum = 2;
+	pinConfig.chBAltNum = 2;
+	encoderList[_Y].EncoderPinInit(pinConfig);
+
+	pinConfig.chAChannel = Gpio::_ChA;
+	pinConfig.chBChannel = Gpio::_ChA;
+	pinConfig.chAPin = 0;
+	pinConfig.chBPin = 1;
+	pinConfig.chAAltNum = 1;
+	pinConfig.chBAltNum = 1;
+	encoderList[_Z].EncoderPinInit(pinConfig);
+
+	for (int i = 0; i < num_encoders; ++i)
+	{
+		encoderList[i].ConfigResolution(encoder_resolution);
+		encoderList[i].Enable();
+		encoderList[i].Reset();
+	}
 }
 
 
@@ -42,6 +88,14 @@ void App::ConfigExtInt()
 ///This function should be called in the main loop
 void App::Execute()
 {
+	uint16_t encCount;
+
+	for (int i = 0; i < num_encoders; ++i)
+	{
+		encCount = encoderList[i].GetCounter();
+		encoderVals[i] += (int16_t)(encCount - oldEncoderCounts[i]);
+		oldEncoderCounts[i] = encCount;
+	}
 }
 
 
