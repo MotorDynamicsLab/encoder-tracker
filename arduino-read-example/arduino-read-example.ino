7408020d22545d5524de8803d1f91ed9507444a9
David Sun <david@ldomotors.com>
2019-12-02 15:11:28 +0800

    modify code to use modern SPI beginTransaction() interface

--- arduino-read-example/arduino-read-example.ino
+++ arduino-read-example/arduino-read-example.ino
@@ -19,12 +19,7 @@ void setup()
   digitalWrite(NSS_PIN, HIGH);
 
   Serial.begin(9600);
-  
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV4);
   SPI.begin();
-  //SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
 }
 
 void loop()
@@ -45,10 +40,15 @@ void loop()
 void ResetEncoder(SpiEncoders encoders)
 {
   const byte reset_cmd = 0x00;
-  
+
+  SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));
   digitalWrite(NSS_PIN,LOW);
+  delayMicroseconds(2);
   SPI.transfer(reset_cmd | (encoders << spi_encoder_offset) );
+  delayMicroseconds(5);
   digitalWrite(NSS_PIN,HIGH);
+  SPI.endTransaction();
+  
 }
 
 long ReadEncoder(SpiEncoders encoder)
@@ -56,7 +56,8 @@ long ReadEncoder(SpiEncoders encoder)
   const byte read_cmd  = 0x0F;
   int readSize = 4;
   long encoderVal = 0;
-  
+
+  SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));
   digitalWrite(NSS_PIN,LOW);
   delayMicroseconds(2);
   SPI.transfer(read_cmd | (encoder << spi_encoder_offset) );
@@ -69,5 +70,6 @@ long ReadEncoder(SpiEncoders encoder)
   }
   
   digitalWrite(NSS_PIN,HIGH);
+  SPI.endTransaction();
   return encoderVal;
 }
